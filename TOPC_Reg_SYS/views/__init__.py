1c8372c4aa31eabbc7e3334216c18cf8429246ff
fuadwasi <fuad15-9400@diu.edu.bd>
2020-02-10 15:56:50 +0600

    Nevbar_fix

--- TOPC_Reg_SYS/views/__init__.py
+++ TOPC_Reg_SYS/views/__init__.py
@@ -1,3 +1,4 @@
 from .main import *
 from .std_reg import *
 from .signup import *
+from .r_set import *
--- TOPC_Reg_SYS/views/main.py
+++ TOPC_Reg_SYS/views/main.py
@@ -44,7 +44,7 @@ def login(request):
             messages.error(request, "Username and Password can't be empty")
             return redirect("login")
 
-        get_user = User.objects.get(Q(username__exact=username))
+        get_user = User.objects.filter(Q(username__exact=username))
 
         if get_user:
             user = authenticate(request, username=username, password=password)
@@ -77,24 +77,24 @@ def ush(request):
         else:
             level = 1
         if request.method == "GET":
-            context = {'level': level, 'stdcount': stdcount, 'user': request.user.username}
+            context = {'level': level, 'stdcount': stdcount, 'user': request.user}
             return render(request, "user_home.html", context)
         elif request.method == "POST":
             sID = request.POST['search']
             if sID == '%':
                 obj = ALLStudents.objects.all()
-                context = {'level': level, 'stdcount': stdcount, 'ALLstudents': obj, 'user': request.user.username}
+                context = {'level': level, 'stdcount': stdcount, 'ALLstudents': obj, 'user': request.user}
                 return render(request, "user_home.html", context)
             elif sID != "":
                 std = ALLStudents.objects.filter(Q(sID__contains=sID) | Q(name__contains=sID))
 
                 if std:
-                    context = {'level': level, 'stdcount': stdcount, 'ALLstudents': std, 'user': request.user.username}
+                    context = {'level': level, 'stdcount': stdcount, 'ALLstudents': std, 'user': request.user}
                     return render(request, "user_home.html", context)
                 else:
                     pass
             else:
-                context = {'level': level, 'stdcount': stdcount, 'user': request.user.username}
+                context = {'level': level, 'stdcount': stdcount, 'user': request.user}
                 return render(request, "user_home.html", context)
     else:
         system_messages = messages.get_messages(request)
@@ -128,68 +128,13 @@ def users(request):
             if request.user.is_superuser:
                 level = 3
                 all_user = User.objects.filter(Q(username=username))
-        context = {'students': all_user, 'level': level, 'user': request.user.username}
+        context = {'students': all_user, 'level': level, 'user': request.user}
         return render(request, 'users.html', context)
     else:
         return redirect("ush")
 
 
-def r_set(request, id):
-    if request.user.is_staff:
-        admin = User.objects.get(Q(id__exact=id))
-        if request.user.is_staff:
-            level = 2
-        if request.user.is_superuser:
-            level = 3
 
-        if request.method == 'GET':
-            context = {'admin': admin.username, 'level': level, 'user': request.user.username}
-
-            return render(request, 'reset_pass.html', context)
-        elif request.method == "POST":
-            system_messages = messages.get_messages(request)
-            for message in system_messages:
-                # This iteration is necessary
-                pass
-            system_messages.used = True
-            pass
-            password = request.POST["password"]
-
-            password2 = request.POST["confirm_password"]
-
-            current_admin_password = request.POST["admin_password"]
-            current_admin = request.user
-            current_user = authenticate(request, username=current_admin.username, password=current_admin_password)
-            if current_user is not None:
-                if request.user.is_staff:
-                    level = 0
-                    if request.user.is_staff:
-                        level = 2
-                    if request.user.is_superuser:
-                        level = 3
-                    if admin.is_superuser <= current_admin.is_superuser:
-                        if password != password2 or password == '':
-                            context = {'admin': admin.username, 'level': level, 'user': request.user.username,
-                                       'messages': "New password is not valid"}
-                            return render(request, "reset_pass.html", context)
-                        else:
-                            admin.set_password(password)
-                            admin.save()
-                            messages.success(request, "Password Change Successfully")
-                            return redirect('users')
-                    else:
-                        messages.error(request, 'You are not authorized')
-                        return redirect('users')
-            else:
-                context = {'admin': admin.username, 'level': level, 'user': request.user.username,
-                           'messages': "Wrong password entered"}
-                return render(request, "reset_pass.html", context)
-
-                context = {'students': all_user, 'level': level, 'user': request.user.username}
-                messages.success(request, )
-                return render(request, 'users.html', context)
-    else:
-        return redirect("ush")
 
 
 def delete_user(request, id):
