23b8cf7888ff799815fa00eb93cff7fa765d074e
fuadwasi <fuad15-9400@diu.edu.bd>
2020-02-11 23:16:47 +0600

    new_model new Table

--- TOPC_Reg_SYS/models.py
+++ TOPC_Reg_SYS/models.py
@@ -14,12 +14,7 @@ class ALLStudents(models.Model):
     shift= models.CharField(max_length=100, default="none")
     email = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
-    t_shirt= models.CharField(max_length=100, default="none")
-    rank = models.IntegerField(default=0)
-    solve = models.IntegerField(default=99)
-    penalty = models.IntegerField(default=99999)
-    regby = models.CharField(max_length=100)
-    regtiem = models.DateTimeField()
+
     status = models.CharField(max_length=100, default="Not_Registered")
 
     class Mate:
@@ -29,15 +24,6 @@ class ALLStudents(models.Model):
 class RegStudents(models.Model):
     token = models.IntegerField(default=99999)
     sID = models.CharField(max_length=30)
-    name = models.CharField(max_length=100)
-    semester = models.CharField(max_length=100)
-    section = models.CharField(max_length=100)
-    department = models.CharField(max_length=100)
-    campus = models.CharField(max_length=100)
-    shift= models.CharField(max_length=100, default="Pending")
-    email = models.CharField(max_length=100)
-    phone = models.CharField(max_length=100)
-    gender= models.CharField(max_length=100, default="none")
     t_shirt= models.CharField(max_length=100, default="none")
     room = models.CharField(max_length=100)
     pc_no = models.CharField(max_length=100)
@@ -49,7 +35,10 @@ class RegStudents(models.Model):
     advance_camp = models.CharField(max_length=100, default="Pending")
     regby = models.CharField(max_length=100)
     regtiem = models.DateTimeField()
-    reg_status = models.CharField(max_length=100, default="Not_Registered")
+    basic_info = models.OneToOneField(ALLStudents, on_delete=models.CASCADE)
+
 
     class Mate:
         db_table = "reg_students"
+
+
