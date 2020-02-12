87a977ec6562cb34e84cc8ebfa432c884a801057
fuadwasi <fuad15-9400@diu.edu.bd>
2020-02-12 15:14:11 +0600

    std_reg delete

--- TOPC_Reg_SYS/views/__init__.py
+++ TOPC_Reg_SYS/views/__init__.py
@@ -1,5 +1,4 @@
 from .main import *
-from .std_reg import *
 from .upload_data import *
 from .signup import *
 from .r_set import *
--- TOPC_Reg_SYS/views/std_reg.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def std_reg(request):
-    return render(request, 'std-reg.html')
