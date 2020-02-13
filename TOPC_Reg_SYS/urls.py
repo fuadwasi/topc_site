ef780720adbb63c41c62c37da62e3eeec974739e
fuadwasi <fuad15-9400@diu.edu.bd>
2020-02-13 16:07:24 +0600

    req command on hello.txt

--- TOPC_Reg_SYS/urls.py
+++ TOPC_Reg_SYS/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path('', views.home, name="home"),
     path('dashboard/', views.dashboard, name="dashboard"),
     path('login/', views.login, name="login"),
